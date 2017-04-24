8d56e394eb681f1aaaf70a33e06f29b2bf0f1a16
Luc Duron <l.duron@cnr.tm.fr>
2017-04-24 18:16:07 +0200

    Remove doxygen output folder

--- doc/html/search/classes_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['myargparse',['myargparse',['../classcommon_1_1arg__command__line_1_1myargparse.html',1,'common::arg_command_line']]]
-];
--- doc/html/search/classes_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['qplaintexteditlogger',['QPlainTextEditLogger',['../classslf__interface_1_1_q_plain_text_edit_logger.html',1,'slf_interface']]]
-];
