Fix bug with max computation

cli/slf_max_over_files.py
@@ -2,7 +2,9 @@
 """
 Compute the max over some variables for a serie of files (which may contain several frames).
 The output file contains a single frame with the variables.
-/!\ Vectors are not considered!
+This tool has some limitations :
+- vectors are not considered as such
+- meshes have to be similar
 """
 import numpy as np
 import sys
@@ -31,6 +33,11 @@ def slf_max_over_files(args):
                 sys.exit(3)
             resin.get_time()
 
+            for var_ID in var_IDs:
+                if var_ID not in resin.header.var_IDs:
+                    logger.critical('The variable %s is missing in %s' % (var_ID, in_slf))
+                    sys.exit(3)
+
             if i == 0:
                 output_header = resin.header.copy()
                 output_header.empty_variables()
@@ -38,19 +45,18 @@ def slf_max_over_files(args):
                     output_header.add_variable_from_ID(var_ID)
                 max_values = np.empty((output_header.nb_var, output_header.nb_nodes),
                                       dtype=output_header.np_float_type)
-
-            for var_ID in var_IDs:
-                if var_ID not in resin.header.var_IDs:
-                    logger.critical('The variable %s is missing in %s' % (var_ID, in_slf))
-                    sys.exit(3)
+            else:
+                if not resin.header.same_2d_mesh(output_header):
+                    logger.critical('The mesh of %s is different from the first one' % (in_slf))
+                    sys.exit(1)
 
             for time_index, time in enumerate(resin.time):
-                for i, var_ID in enumerate(var_IDs):
+                for j, var_ID in enumerate(var_IDs):
                     values = resin.read_var_in_frame(time_index, var_ID)
-                    if time_index == 0:
-                        max_values[i, :] = values
+                    if time_index == 0 and i == 0:
+                        max_values[j, :] = values
                     else:
-                        max_values[i, :] = np.max(max_values[i, :], values)
+                        max_values[j, :] = np.maximum(max_values[j, :], values)
 
     with Serafin.Write(args.out_slf, args.lang, overwrite=args.force) as resout:
         resout.write_header(output_header)